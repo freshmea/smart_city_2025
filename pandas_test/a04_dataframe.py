467e2e4b9ae68cb87928dade857db95ed20682a1
freshmea <freshmea@gmail.com>
2025-10-23 11:04:31 +0900

    aa

--- pandas_test/a04_dataframe.py
+++ pandas_test/a04_dataframe.py
@@ -1,11 +1,12 @@
 import pandas as pd
+from util import pd_show
 
 
 def main():
     df = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-    print(df)
+    pd_show("df", df)
     df2 = pd.DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=['가', '나', '다'])
-    print(df2)
+    pd_show("df2", df2)
 
     data = {
     'name': ['Kim', 'Lee', 'Park'],
@@ -13,7 +14,7 @@ def main():
     'children': [2, 1, 3]
     }
     df3 = pd.DataFrame(data)
-    print(df3)
+    pd_show("df3", df3)
 
 if __name__ == "__main__":
     main()
--- pandas_test/util.py
+++ pandas_test/util.py
@@ -1,5 +1,9 @@
 import numpy as np
+import pandas as pd
 
 
 def show(name : str, arr : np.ndarray):
-    print(f"{name} : {arr}, shape: {arr.shape}, dtype: {arr.dtype}")
+    print(f"{name} :\n{arr}, shape: {arr.shape}, dtype: {arr.dtype}")
+
+def pd_show(name : str, arr : pd.DataFrame):
+    print(f"{name} :\n{arr}, shape: {arr.shape}, dtype: \n{arr.dtypes}")
