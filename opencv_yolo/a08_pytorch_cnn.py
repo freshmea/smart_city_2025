8cad3f23dfed0df3ace136d5d623c0d7f17ec8ec
freshmea <freshmea@gmail.com>
2025-10-24 14:50:06 +0900

    lenet

--- opencv_yolo/a08_pytorch_cnn.py
+++ opencv_yolo/a08_pytorch_cnn.py
@@ -1,3 +1,71 @@
 # pip install torch torchvision torchaudio
+import os
+import random
 
+import cv2
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from matplotlib import pyplot as plt
+from PIL import Image
+from torch import optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader, Dataset
+from torchvision import transforms
+from tqdm import tqdm_notebook as tqdm
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
+class ImageTransform():
+    def __init__(self, resize, mean, std):
+        self.data_transform = {
+            'train': transforms.Compose([
+                transforms.RandomResizedCrop(resize, scale=(0.5, 1.0)),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ]),
+            'val': transforms.Compose([
+                transforms.Resize(256),
+                transforms.CenterCrop(resize),
+                transforms.ToTensor(),
+                transforms.Normalize(mean, std)
+            ])
+        }
+
+    def __call__(self, img, phase):
+        return self.data_transform[phase](img)
+
+class LeNet(nn.Module):
+    def __init__(self):
+        super(LeNet, self).__init__()
+        self.cnn1 = nn.Conv2d(in_channels=3, out_channels=16, kernel_size=5, stride=1, padding=0)
+        self.relu1 = nn.ReLU()
+        self.maxpool1 = nn.MaxPool2d(kernel_size=2)
+        self.cnn2 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=5, stride=1, padding=0)
+        self.relu2 = nn.ReLU() # activation
+        self.maxpool2 = nn.MaxPool2d(kernel_size=2)
+        self.fc1 = nn.Linear(32*53*53, 512)
+        self.relu5 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 2)
+        self.output = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        out = self.cnn1(x)
+        out = self.relu1(out)
+        out = self.maxpool1(out)
+        out = self.cnn2(out)
+        out = self.relu2(out)
+        out = self.maxpool2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc1(out)
+        out = self.relu5(out)
+        out = self.fc2(out)
+        out = self.output(out)
+        return out
+
+model = LeNet()
+print(model)
+
