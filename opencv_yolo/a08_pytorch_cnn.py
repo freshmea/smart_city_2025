e10b0c47bfe6109723e8fc0e4f0591f1ee9959ea
freshmea <freshmea@gmail.com>
2025-10-24 15:42:52 +0900

    cnn train

--- opencv_yolo/a08_pytorch_cnn.py
+++ opencv_yolo/a08_pytorch_cnn.py
@@ -77,7 +77,9 @@ dog_images_filepaths = sorted([os.path.join(dog_directory, f) for f in os.listdi
 images_filepaths = [*cat_images_filepaths, *dog_images_filepaths]
 correct_images_filepaths = [i for i in images_filepaths if cv2.imread(i) is not None]
 
-random.seed(42)
+import time
+
+random.seed(time.time())
 random.shuffle(correct_images_filepaths)
 #train_images_filepaths = correct_images_filepaths[:20000] #성능을 향상시키고 싶다면 훈련 데이터셋을 늘려서 테스트해보세요
 #val_images_filepaths = correct_images_filepaths[20000:-10] #훈련과 함께 검증도 늘려줘야 합니다
@@ -85,3 +87,140 @@ train_images_filepaths = correct_images_filepaths[:400]
 val_images_filepaths = correct_images_filepaths[400:-10]
 test_images_filepaths = correct_images_filepaths[-10:]
 print(len(train_images_filepaths), len(val_images_filepaths), len(test_images_filepaths))
+
+def display_image_grid(images_filepaths, predicted_labels=(), cols=5):
+    rows = len(images_filepaths) // cols
+    figure, ax = plt.subplots(nrows=rows, ncols=cols, figsize=(12, 6))
+    for i, image_filepath in enumerate(images_filepaths):
+        image = cv2.imread(image_filepath)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        true_label = os.path.normpath(image_filepath).split(os.sep)[-2]
+        predicted_label = predicted_labels[i] if predicted_labels else true_label
+        color = "green" if true_label == predicted_label else "red"
+        ax.ravel()[i].imshow(image)
+        ax.ravel()[i].set_title(predicted_label, color=color)
+        ax.ravel()[i].set_axis_off()
+    plt.tight_layout()
+    plt.show()
+
+# sudo apt-get install -y python3-tk
+import matplotlib
+
+matplotlib.use('TkAgg')
+display_image_grid(test_images_filepaths)
+
+# 하이퍼파라미터 설정
+size = 224
+mean = (0.485, 0.456, 0.406)
+std = (0.229, 0.224, 0.225)
+batch_size = 32
+
+class DogvsCatDataset(Dataset):
+    def __init__(self, file_list, transform=None, phase='train'):
+        self.file_list = file_list
+        self.transform = transform
+        self.phase = phase
+
+    def __len__(self):
+        return len(self.file_list)
+
+    def __getitem__(self, idx):
+        img_path = self.file_list[idx]
+        img = Image.open(img_path)
+        img_transformed = self.transform(img, self.phase)
+
+        label = img_path.split('/')[-1].split('.')[0]
+        if label == 'dog':
+            label = 1
+        elif label == 'cat':
+            label = 0
+        return img_transformed, label
+
+train_dataset = DogvsCatDataset(train_images_filepaths, transform=ImageTransform(size, mean, std), phase='train')
+val_dataset = DogvsCatDataset(val_images_filepaths, transform=ImageTransform(size, mean, std), phase='val')
+
+index = 0
+print(train_dataset.__getitem__(index)[0].size())
+print(train_dataset.__getitem__(index)[1])
+
+# data loader 만들기
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_dataloader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+dataloader_dict = {'train': train_dataloader, 'val': val_dataloader}
+
+batch_iterator = iter(train_dataloader)
+inputs, label = next(batch_iterator)
+print(inputs.size())
+print(label)
+
+# pip install torchsummary
+from torchsummary import summary
+
+summary(model, input_size=(3, 224, 224))
+
+# 파라미터 보기.
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+print(f'The model has {count_parameters(model):,} trainable parameters')
+
+# 옵티마이저 세팅
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+criterion = nn.CrossEntropyLoss()
+
+model = model.to(device)
+criterion = criterion.to(device)
+
+def train_model(model, dataloader_dict, criterion, optimizer, num_epoch):
+    since = time.time()
+    best_acc = 0.0
+
+    for epoch in range(num_epoch):
+        print('Epoch {}/{}'.format(epoch + 1, num_epoch))
+        print('-'*20)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()
+            else:
+                model.eval()
+
+            epoch_loss = 0.0
+            epoch_corrects = 0
+
+            for inputs, labels in tqdm(dataloader_dict[phase]):
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                    epoch_loss += loss.item() * inputs.size(0)
+                    epoch_corrects += torch.sum(preds == labels.data)
+
+            epoch_loss = epoch_loss / len(dataloader_dict[phase].dataset)
+            epoch_acc = epoch_corrects.double() / len(dataloader_dict[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = model.state_dict()
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(
+        time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+    return model
+
+import time
+
+num_epoch = 10
+model = train_model(model, dataloader_dict, criterion, optimizer, num_epoch)
