3669f7ee051dc1f68cf6b87796e992534beaf0bc
freshmea <freshmea@gmail.com>
2025-10-29 11:35:05 +0900

    mediapipe

---
+++ opencv_yolo/yolo/a15_mediapipe.py
@@ -0,0 +1,14 @@
+# uv add mediapipe
+import cv2
+import numpy as np
+
+# import Handpose from mediapipe
+from mediapipe.python.solutions.hands import Hands
+
+
+def main():
+
+    pass
+
+if __name__ == "__main__":
+    main()
