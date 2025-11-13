0dfdb0ce23d6e7c24ddb7f184711fcedaf6c96f3
freshmea <freshmea@gmail.com>
2025-11-13 13:11:45 +0900

    pose

--- yolo_uv/a03_yolo_seg.py
+++ yolo_uv/a03_yolo_seg.py
@@ -15,12 +15,14 @@ def main():
     model = YOLO("yolov8n-seg.pt")
     path = folder_path + "cat.4.jpg"
     img = cv2.imread(path)
+    print("img shape : " + str(img.shape))
     results = model.predict(img, verbose=True)
     res = results[0]
-    print(type(results))
-    print(results)
-    print(results[0].boxes)
-    print(results[0].masks)
+
+    # print(type(results))
+    # print(results)
+    # print(results[0].boxes)
+    # print(results[0].masks)
 
     # yolo 제공 그리기
     # annotated_frame = res.plot()
@@ -37,14 +39,17 @@ def main():
     # mask visualization
     for label, idx in zip(class_info, index_info):
         mask = res.masks.data[idx].cpu().numpy()
+        print("mask shape : " + str(mask.shape))
         # 처리
         kernel = np.ones((15,15), np.uint8)
         mask = cv2.morphologyEx(mask.astype(np.uint8), cv2.MORPH_CLOSE, kernel, iterations=2).astype(bool)
-        cv2.dilate(mask.astype(np.uint8), kernel, iterations=3)
+        print("mask shape : " + str(mask.shape))
+        mask = cv2.dilate(mask.astype(np.uint8), kernel, iterations=3)
         yellow = np.full_like(img, (0,0,0))
         # black = np.zeros_like(img)
         # mask 의 사이즈를 img 와 똑같이 crop 을 이용해서 맞춰줘야함
         mask = cv2.resize(mask.astype(np.uint8), (img.shape[1], img.shape[0]), interpolation=cv2.INTER_NEAREST).astype(bool)
+        print("mask shape : " + str(mask.shape))
         frame = np.where(mask[..., None], yellow, img)
     annotated_frame = frame
 
